ca5607e515ff18687010f01369260b39f8cf929d
Abhishek Nath <oovnath2@gmail.com>
2025-08-16 21:06:24 +0530

    update slab

--- slab/urls.py
+++ slab/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # path('daily-earning/', views.daily_earning_view),
     # path('team-earning/', views.team_earning_view),
+    path('earning_list_by_user/', views.earning_list_by_user),
 ]
